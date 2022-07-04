d88fbbbb9ce53905fd0038a4b7fba4cf6a1c4da5
Elson Otake <elsonotake@yahoo.com.br>
2022-07-04 16:58:46 -0300

    Update applying destructuring

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,6 +4,7 @@ import styles from "./TodoItem.module.css"
 class TodoItem extends React.Component {
 
   render() {
+    const { completed, id, title } = this.props.todo
     const completedStyle = {
       fontStyle: "italic",
       color: "#595959",
@@ -15,14 +16,14 @@ class TodoItem extends React.Component {
         <input
           type="checkbox"
           className={styles.checkbox}
-          checked={this.props.todo.completed}
-          onChange={() => this.props.handleChangeProps(this.props.todo.id)}
+          checked={completed}
+          onChange={() => this.props.handleChangeProps(id)}
         />
-        <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
+        <button onClick={() => this.props.deleteTodoProps(id)}>
           Delete
         </button>
-        <span style={this.props.todo.completed ? completedStyle : null}>
-          {this.props.todo.title}
+        <span style={completed ? completedStyle : null}>
+          {title}
         </span>
       </li>
     );

14a301f173c947f9d340f606b8d11e97d884748e
Elson Otake <elsonotake@yahoo.com.br>
2022-07-04 15:56:33 -0300

    Add style file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import TodoContainer from "./components/TodoContainer";
-import './index.css';
+import './App.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(

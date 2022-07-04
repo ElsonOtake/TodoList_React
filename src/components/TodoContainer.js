79ad6bd0b975fde029c232ce64554fb8ee265004
Elson Otake <elsonotake@yahoo.com.br>
2022-07-04 13:13:30 -0300

    Update list of tasks

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,12 +1,33 @@
 import React from "react";
 
 class TodoContainer extends React.Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "Setup development environment",
+        completed: true
+      },
+      {
+        id: 2,
+        title: "Develop website and add content",
+        completed: false
+      },
+      {
+        id: 3,
+        title: "Deploy to live server",
+        completed: false
+      }
+    ]
+   };
+
   render() {
     return (
-      <>
-        <h1>Hello from Create React App</h1>
-        <p>I am in a React Component!</p>
-      </>
+      <ul>
+        {this.state.todos.map(todo => (
+          <li>{todo.title}</li>
+        ))}
+      </ul>
     )
   }
 };

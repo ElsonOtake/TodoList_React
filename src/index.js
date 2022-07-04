491fc8f55525cd5be8cc08d93591a6ac4518904b
Elson Otake <elsonotake@yahoo.com.br>
2022-07-04 13:05:51 -0300

    Add TodoContainer

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import TodoContainer from "./components/TodoContainer"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <h1>Hello from Create React App</h1>
+  <TodoContainer />
 );
 

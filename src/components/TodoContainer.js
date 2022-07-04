e3cca79247433ebcd51e8bfc86ff09c9866f4604
Elson Otake <elsonotake@yahoo.com.br>
2022-07-04 13:22:09 -0300

    Add key to list

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import TodosList from "./TodosList";
 
 class TodoContainer extends React.Component {
   state = {
@@ -23,12 +24,8 @@ class TodoContainer extends React.Component {
 
   render() {
     return (
-      <ul>
-        {this.state.todos.map(todo => (
-          <li>{todo.title}</li>
-        ))}
-      </ul>
-    )
+      <TodosList todos={this.state.todos} />
+    );
   }
 };
 

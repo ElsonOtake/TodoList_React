ec4c7e0ea4e320ee0ac669a7d93d9fecb22abf81
Elson Otake <elsonotake@yahoo.com.br>
2022-07-04 19:36:29 -0300

    Update tests unmount

--- src/functionBased/components/TodoItem.js
+++ src/functionBased/components/TodoItem.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from "./TodoItem.module.css"
 
 const TodoItem = props => {
@@ -20,7 +20,7 @@ const TodoItem = props => {
     opacity: 0.4,
     textDecoration: "line-through",
   }
-  
+
   let viewMode = {}
   let editMode = {}
 
@@ -29,6 +29,12 @@ const TodoItem = props => {
   } else {
     editMode.display = "none"
   }
+
+  useEffect(() => {
+    return () => {
+      console.log("Cleaning up...")
+    }
+  }, []);
 
   return (
     <li className={styles.item}>

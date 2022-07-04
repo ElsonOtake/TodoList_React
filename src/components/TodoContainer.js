2553bec5af3aabc977fdd8bd641d932cd16c4f5e
Elson Otake <elsonotake@yahoo.com.br>
2022-07-04 17:48:10 -0300

    Update copy todos from jsonplaceholder

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -53,6 +53,12 @@ class TodoContainer extends React.Component {
     });
   };
 
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/todos?_limit=10")
+      .then(response => response.json())
+      .then(data => this.setState({ todos: data }));
+  }
+
   render() {
     return (
       <div className="container">

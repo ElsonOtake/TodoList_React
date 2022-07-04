afadb60b3a8e1ba7b396884077c9612d13534483
Elson Otake <elsonotake@yahoo.com.br>
2022-07-04 13:32:12 -0300

    Update TodoItem to function

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,9 +1,5 @@
 import React from "react";
 
-class TodoItem extends React.Component {
-  render() {
-    return <li>{this.props.todo.title}</li>
-  }
-};
+const TodoItem = (props) => <li>{props.todo.title}</li>;
 
 export default TodoItem;

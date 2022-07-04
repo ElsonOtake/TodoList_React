825f1e287b5e6fbd88d50475bd2bfbbb7937f374
Elson Otake <elsonotake@yahoo.com.br>
2022-07-04 14:26:20 -0300

    Update input onchange event

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -1,10 +1,23 @@
 import React, { Component } from "react";
 
 class InputTodo extends Component {
+  state = {
+    title: ""
+  };
+
+  onChange = e => {
+    console.log("hello");
+  };
+  
   render() {
     return (
       <form>
-        <input type="text" placeholder="Add Todo..." />
+        <input
+          type="text"
+          placeholder="Add Todo..."
+          value={this.state.title}
+          onChange={this.onChange}
+        />
         <button>Submit</button>
       </form>
     );

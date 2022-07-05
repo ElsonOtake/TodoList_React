304f744f44ba6c90dc31656876d7ae7c797edbe2
Elson Otake <elsonotake@yahoo.com.br>
2022-07-05 12:17:15 -0300

    Update about content

--- src/functionBased/pages/About.js
+++ src/functionBased/pages/About.js
@@ -1,17 +1,58 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 const About = () => (
-  <div>
-    <ul>
-      <li>
-        <Link to="/about-app">About App</Link>
-      </li>
-      <li>
-        <Link to="/about-author">About Author</Link>
-      </li>
-    </ul>
-  </div>
+  <header>
+    <h1
+      style={{
+        fontSize: '6rem',
+        fontWeight: '600',
+        marginTop: '4rem',
+        marginBottom: '2rem',
+        lineHeight: '1em',
+        color: '#888',
+        textTransform: 'lowercase',
+        textAlign: 'center',
+      }}
+    >
+      About
+    </h1>
+    <h2
+      style={{
+        fontSize: '4rem',
+        fontWeight: '600',
+        marginBottom: '4rem',
+        lineHeight: '1em',
+        color: '#888',
+        textTransform: 'lowercase',
+        textAlign: 'center',
+      }}
+    >
+      Todo List
+    </h2>
+    <p
+      style={{
+        fontSize: '2rem',
+        fontWeight: '600',
+        lineHeight: '1em',
+        color: '#888',
+        textTransform: 'lowercase',
+        textAlign: 'center',
+      }}
+    >
+      &copy; Copyright EAOrigami Inc. 2022
+    </p>
+  </header>
+  // <div>
+  //  /* <ul>
+  //    <li>
+  //      <Link to="/about-app">About App</Link>
+  //    </li>
+  //    <li>
+  //      <Link to="/about-author">About Author</Link>
+  //    </li>
+  //  </ul> */
+  // </div>
 );
 
 export default About;

aa8c028f2e46b45905162a7a200334070a06d163
Elson Otake <elsonotake@yahoo.com.br>
2022-07-04 22:00:14 -0300

    Update Routes

--- src/index.js
+++ src/index.js
@@ -2,13 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import TodoContainer from './functionBased/components/TodoContainer';
 import './functionBased/App.css';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './functionBased/components/Navbar';
+import About from './functionBased/pages/About';
+import NotMatch from './functionBased/pages/NotMatch';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-      <TodoContainer />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<TodoContainer />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotMatch />} />
+      </Routes>
     </Router>
   </React.StrictMode>,
 );
